2ebe557b09fc7442bc16a0218911c08541725056
Bisista Shrestha <sbisista@gmail.com>
2026-07-24 15:44:32 +0530

    Created User Model & setup admin site.

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+from .forms import CustomUserChangeForm, CustomUserCreationForm
 
 # Register your models here.
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    list_display=[
+        "email",
+        "first_name",
+        "last_name",
+        "phone_number",
+        "is_staff",
+        "is_active",
+        "date_joined"
+    ]
+    search_fields=[
+        "email",
+        "first_name",
+        "last_name",
+        "phone_number"
+    ]
+    list_filter=[
+        "is_staff",
+        "is_active",
+        "date_joined",
+        "groups"
+    ]
+    ordering = ("-date_joined",)
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Personal info", {"fields": ("first_name", "last_name","phone_number")}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "first_name", "last_name", "phone_number", "password1", "password2"),
+        }),
+    )
