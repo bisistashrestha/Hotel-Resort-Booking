4686378587cd56510a7cf341a931037e26e53f8a
Bisista Shrestha <sbisista@gmail.com>
2026-07-27 13:18:14 +0530

    JWT Token Login and tokens

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RegisterView
+from .views import RegisterView, UserProfileView
 from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView)
 
 urlpatterns = [
@@ -8,4 +8,6 @@ urlpatterns = [
     path("login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+
+    path("profile/", UserProfileView.as_view(), name="profile"),
 ]
--- backend/users/views.py
+++ backend/users/views.py
@@ -4,6 +4,19 @@ from rest_framework import generics
 from rest_framework.permissions import AllowAny
 from .serializers import UserRegistrationSerializer
 
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
+class UserProfileView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        return Response({
+            "message": "You made it into the VIP Lounge!",
+            "user_email": request.user.email
+        })
+
 class RegisterView(generics.CreateAPIView):
     serializer_class = UserRegistrationSerializer
     permission_classes = (AllowAny, )
