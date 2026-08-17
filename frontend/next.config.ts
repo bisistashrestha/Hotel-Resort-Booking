a63ed8118906316c2f50bb7c7a75a680da6a7e91
Bisista Shrestha <sbisista@gmail.com>
2026-08-17 09:59:00 +0530

    Update

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,13 +1,22 @@
 import type { NextConfig } from "next";
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 const nextConfig: NextConfig = {};
 =======
+=======
+>>>>>>> 859be5c (Fine tuned and added secure cookies)
 const nextConfig: NextConfig = {
 	turbopack: {
 		root: __dirname,
 	},
 };
+<<<<<<< HEAD
 >>>>>>> 25b18e9 (Revert)
+=======
+=======
+const nextConfig: NextConfig = {};
+>>>>>>> 6174ceb (Fine tuned and added secure cookies)
+>>>>>>> 859be5c (Fine tuned and added secure cookies)
 
 export default nextConfig;
