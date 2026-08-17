859be5c7b9155a253b96d75c18ba577d6053dc5f
Bisista Shrestha <sbisista@gmail.com>
2026-08-17 09:59:00 +0530

    Fine tuned and added secure cookies

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,9 +1,13 @@
 import type { NextConfig } from "next";
 
+<<<<<<< HEAD
 const nextConfig: NextConfig = {
 	turbopack: {
 		root: __dirname,
 	},
 };
+=======
+const nextConfig: NextConfig = {};
+>>>>>>> 6174ceb (Fine tuned and added secure cookies)
 
 export default nextConfig;
